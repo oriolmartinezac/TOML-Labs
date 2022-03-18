d90f27c589156b102267efb1e4c0a0504b1c83df
oriolmartinezac <omartinez.roser@gmail.com>
2022-03-18 16:02:53 +0100

    Exercise_7 finished

--- project-1/Exercise_7.py
+++ project-1/Exercise_7.py
@@ -16,10 +16,7 @@ obj_fun = cvxpy.log(x[0]) + cvxpy.log(x[1]) + cvxpy.log(x[2])
 cons1 = x[0] + x[2]
 cons2 = x[0] + x[1]
 cons3 = x[2]
-cons4 = x[0]
-cons5 = x[1]
-cons6 = x[2]
-constraints = [cons1 <= c[0], cons2 <= c[1], cons3 <= c[4], cons4 >= 0, cons5 >= 0, cons6 >= 0]
+constraints = [cons1 <= c[0], cons2 <= c[1], cons3 <= c[4], x[0] >= 0, x[1] >= 0, x[2] >= 0]
 
 # Form and solve problem.
 prob = Problem(Maximize(obj_fun), constraints)
@@ -28,3 +25,8 @@ print ("prob1 is DCP:", prob.is_dcp())
 print("status:", prob.status)
 print("optimal value p* = ", prob.value)
 print("optimal var: x1 = ", x[0].value, " var x2: ", x[1].value, " var x3: ", x[2].value)
+print("optimal dual variables lambda1 = ", constraints[0].dual_value)
+print("optimal dual variables lambda2 = ", constraints[1].dual_value)
+print("optimal dual variables lambda3 = ", constraints[2].dual_value)
+print("optimal dual variables lambda4 = ", constraints[3].dual_value)
+print("optimal dual variables lambda5 = ", constraints[4].dual_value)
