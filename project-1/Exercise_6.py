261e3affb72d67fbb99f2573f6ee5cb68dc146ea
oriolmartinezac <omartinez.roser@gmail.com>
2022-03-18 15:58:12 +0100

    Beauty code

--- project-1/Exercise_6.py
+++ project-1/Exercise_6.py
@@ -17,48 +17,66 @@ def obj_fun2():
 def plot_fun2(x):
     return 2*(x**4) - 4 * (x**2) + x - 0.5
 
-def gradient_cal(obj_fun, stop_criteria, max_iterations, initial_guess, method, l_rate=0.000001):
-    if method == "backtracking":
-        cur_x = initial_guess
-        i = 0
-        diff = 1.0
-        grad_fun = nd.Gradient(obj_fun)
-        while i < max_iterations and diff > stop_criteria:
-            prev_x = cur_x
-            cur_x = cur_x - l_rate * grad_fun(prev_x)
-            diff = abs(cur_x - prev_x)
-            i = i + 1
-            print("Iteration", i, "\nX value is", cur_x)
+def gradient(obj_fun, stop_criteria, max_iterations, initial_guess, l_rate=0.01):
+    cur_x = initial_guess
+    i = 0
+    diff = 1.0
+    grad_fun = nd.Gradient(obj_fun)
 
-        print("The local minimum occurs at", cur_x)
-        return cur_x
+    print("Initial value: ", cur_x)
 
-def newton(obj_fun, stop_criteria, max_iterations, initial_guess, l_rate):
+    while i < max_iterations and diff > stop_criteria:
+        prev_x = cur_x
+        cur_x = cur_x - l_rate * grad_fun(prev_x)
+        diff = abs(cur_x - prev_x)
+        i = i + 1
+
+    print("SOLUTION FOUND")
+    print("Number of total iterations", i)
+    print("The local minimum occurs at", cur_x)
+    return cur_x
+
+def newton(obj_fun, stop_criteria, max_iterations, initial_guess, l_rate=0.01):
     grad = nd.Gradient(obj_fun)
     hess = nd.Hessian(obj_fun)
 
     cur_x = initial_guess
     diff = 1.0
     i = 0
+
+    print("Initial value: ", cur_x)
+
     while i < max_iterations and diff > stop_criteria:
         prev_x = cur_x
         cur_x = cur_x - l_rate * np.linalg.cond(hess(prev_x)) * grad(prev_x) #ill-conditioned
         diff = abs(cur_x - prev_x)
         i = i+1
-        print("Iteration", i, "\nX value is", cur_x)
 
+    print("SOLUTION FOUND")
+    print("Number of total iterations", i)
     print("The local minimum occurs at", cur_x)
     return cur_x
 
+def solver(obj_fun, stop_criteria, max_iterations, initial_guess, l_rate=0.01, method=""):
+    if method == "backtracking":
+        x_result = gradient(obj_fun, stop_criteria, max_iterations, initial_guess, l_rate)
+    elif method == "newton":
+        x_result = newton(obj_fun, stop_criteria, max_iterations, initial_guess, l_rate)
+    else:
+        x_result = None
+    return x_result
+
 if __name__ == "__main__":
+
     stop_criteria = 0.0001
     max_iterations = 10000
     learning_rate = 0.01
 
     x0 = 3
     obj_fun1 = obj_fun1()
-
-    x = gradient_cal(obj_fun1, stop_criteria, max_iterations, x0, "backtracking", learning_rate) #1. objective function, 2.stop criteria, 3. max iterations, 4. Inital_guess 5. Method (Backtracking's method, Newton's method)
+    print("BACKTRACKING")
+    x = solver(obj_fun1, stop_criteria, max_iterations, x0, learning_rate, "backtracking")  #1. objective function, 2.stop criteria, 3. max iterations, 4. Inital_guess 5. Method (Backtracking's method, Newton's method)
+    print("\n")
 
     # PLOT in 2D
     x_dummy1 = np.linspace(-10, 10, 1000)
@@ -68,8 +86,9 @@ if __name__ == "__main__":
     plt.scatter(x, plot_fun1(x), color='red', marker='x', label='opt')
 
     plt.show()
-
-    x = newton(obj_fun1, stop_criteria, max_iterations, x0, learning_rate)
+    print("NEWTON")
+    x = solver(obj_fun1, stop_criteria, max_iterations, x0, learning_rate, "newton")
+    print("\n")
 
     # PLOT in 2D
     x_dummy1 = np.linspace(-10, 10, 1000)
@@ -84,8 +103,8 @@ if __name__ == "__main__":
     obj_fun2 = obj_fun2()
 
     for element in x0:
-        print("ITERATION")
-        x = gradient_cal(obj_fun2, stop_criteria, max_iterations, element, "backtracking", learning_rate)
+        print("BACKTRACKING")
+        x = solver(obj_fun2, stop_criteria, max_iterations, element, learning_rate, "backtracking")
         print("\n")
 
         # PLOT in 2D
@@ -97,9 +116,8 @@ if __name__ == "__main__":
 
         plt.show()
 
-    for element in x0:
-        print("ITERATION")
-        x = newton(obj_fun2, stop_criteria, max_iterations, element, learning_rate)
+        print("NEWTON")
+        x = solver(obj_fun2, stop_criteria, max_iterations, element, learning_rate, "newton")
         print("X: ", x)
         print("\n")
 
