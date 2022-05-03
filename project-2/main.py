8d1d5fa5048f1dfd4d00ded8c925a43c6beabd42
oriolmartinezac <omartinez.roser@gmail.com>
2022-05-03 17:29:01 +0200

    Few changes in the code

--- project-2/main.py
+++ project-2/main.py
@@ -1,6 +1,7 @@
 from header import *  # IMPORTING HEADER FILE
 import matplotlib.pyplot as plt
 import cvxpy
+import math
 
 
 def calc_n_d(d):
@@ -21,7 +22,7 @@ def calc_i_d(d):
 
 
 def calc_f_out(d):
-    f_out = Fs * ((D ** 2 - d ** 2 + 2 * d - 1) / (2 * d - 1))
+    f_out = Fs * ((D ** 2 - (d ** 2) + 2 * d - 1) / (2 * d - 1))
     if d == D:
         f_out = Fs
     return f_out
@@ -32,7 +33,7 @@ def calc_f_b(d):
 
 
 def calc_f_i(d):
-    f_i = Fs * ((D ** 2 - d ** 2) / (2 * d - 1))
+    f_i = Fs * ((D ** 2 - (d ** 2)) / (2 * d - 1))
     if d == 0:
         f_i = Fs * (D ** 2) * C
     return f_i
@@ -63,6 +64,7 @@ def delay_fun(tw):
 
 
 if __name__ == "__main__":
+    # PART 1 #
     time = [1, 5, 10, 15, 20, 25]
     alpha_1, alpha_2, alpha_3 = 0.0, 0.0, 0.0
     x = np.linspace(Tw_min, Tw_max)
@@ -97,14 +99,14 @@ if __name__ == "__main__":
         plt.title("Energy-Delay function with " + title)
         plt.show()
 
-###### 2. PART
-x = cvxpy.Variable(1, name='x')
+# PART 2 #
+x = cvxpy.Variable(1, name='x', pos=True)
 
 alpha1, alpha2, alpha3 = calc_alphas(1)
 beta1, beta2 = calc_betas(D)
 
-Tt_x = (x[0] / (Tps + Tal)) * ((Tps + Tal) / 2) + Tack + Tdata
-E1_tx = (Tcs + Tal + Tt_x) * calc_f_i(1)
+Tt_x = cvxpy.ceil(x[0] / (Tps + Tal)) * ((Tps + Tal) / 2) + Tack + Tdata
+E1_tx = (Tcs + Tal + Tt_x) * calc_f_out(1)
 
 prob1_solves = []
 np_L = np.linspace(100, 5000)
@@ -115,16 +117,21 @@ for l_item in np_L:
     cons2 = x[0]
     cons3 = abs(calc_i_d(0)) * E1_tx
     constraints = [cons1 <= l_item, cons2 >= Tw_min, cons3 <= (1 / 4)]
-
     prob1 = cvxpy.Problem(cvxpy.Minimize(obj_fun1), constraints)
-    prob1_solves.append(prob1.solve())
-    print("solve", prob1.solve())  # Returns the optimal value.
+    prob1_solves.append(prob1.solve(gp=True))
+    #print("solve", prob1.solve())  # Returns the optimal value.
+
+print(prob1_solves)
 
 plt.plot(np_L, prob1_solves, color="blue")
 plt.xlabel('L_max')
 plt.ylabel('Minimization')
 plt.title("Problem 1 to optimize")
 plt.show()
+print("The best solution of Tw in Energy function is", min(prob1_solves))
+L_worst = prob1_solves[0]
+#print(E_worst)
+#print("Te worst solution of Tw in Energy function is", E_worst)
 
 prob2_solves = []
 np_E = np.linspace(0.5, 5)
@@ -137,10 +144,67 @@ for e_item in np_E:
     constraints = [cons1 <= e_item, cons2 >= Tw_min, cons3 <= (1 / 4)]
     prob2 = cvxpy.Problem(cvxpy.Minimize(obj_fun2), constraints)
     prob2_solves.append(prob2.solve())
-    print("solve", prob2.solve())  # Returns the optimal value.
+    # print("solve", prob2.solve())  # Returns the optimal value.
 
+print(prob2_solves)
 plt.plot(np_E, prob2_solves, color="blue")
 plt.xlabel('E_budget')
 plt.ylabel('Minimization')
 plt.title("Problem 2 to optimize")
 plt.show()
+#print("The best solution of Tw in Delay function is", min(prob2_solves))
+#E_worst = prob1_solves[-1]
+#print("Te worst solution of Tw in Delay function is", L_worst)
+
+E_worst = max(energy_fun(np_E))
+L_worst = min(delay_fun(np_L))
+
+plt.plot(energy_fun(np_E), delay_fun(np_L), color="green")
+
+plt.xlabel('Energy')
+plt.ylabel('Delay')
+plt.title("Energy-Delay function")
+plt.show()
+
+print(E_worst)
+print(L_worst)
+
+# PART 3 #
+# Game
+x = cvxpy.Variable(3, name='x')
+Tt_x_3 = (x[2] / (Tps + Tal)) * ((Tps + Tal) / 2) + Tack + Tdata
+E1_tx_3 = (Tcs + Tal + Tt_x_3) * calc_f_out(1)
+obj_fun_game = (cvxpy.log(E_worst - x[0]) + cvxpy.log(L_worst - x[1]))
+cons1 = E_worst
+cons2 = x[0]
+cons3 = L_worst
+cons4 = x[1]
+cons5 = x[2]
+cons6 = abs(calc_i_d(0)) * E1_tx_3
+constraints = [cons1 >= (alpha1 * cvxpy.power(x[2], -1) + alpha2 * x[2] + alpha3),
+               cons2 >= (alpha1 * cvxpy.power(x[2], -1) + alpha2 * x[2] + alpha3),
+               cons3 >= (beta1 * x[2] + beta2),
+               cons4 >= (beta1 * x[2] + beta2),
+               cons5 >= Tw_min,
+               cons6 <= (1 / 4)]
+
+prob3 = cvxpy.Problem(cvxpy.Maximize(obj_fun_game), constraints)
+try:
+    result = prob3.solve()
+except cvxpy.SolverError:
+    result = prob3.solve(solver=cvxpy.SCS)
+
+print("optimal value p* = ", prob3.value)
+print("optimal var: E_1 = ", x[0].value)
+print("optimal var: L_1 = ", x[1].value)
+print("optimal var: T_w = ", x[2].value)
+
+plt.plot(energy_fun(np_E), delay_fun(np_L), color="green")
+
+
+plt.xlabel('Energy')
+plt.ylabel('Delay')
+plt.title("Energy-Delay function")
+plt.show()
+
+plt.plot(x[0].value, x[1].value, 'o', color='red')
